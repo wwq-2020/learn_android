ed257e6ab63ea4dbe8f998083c85116d0855f0e2
wwq <2993895632@qq.com>
2020-09-23 11:05:14 +0800

    feat: learn android

---
+++ app/src/main/java/com/example/devilsm/diceroller/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.devilsm.diceroller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import  android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImage:ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton:Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            ollDice()
+        }
+        diceImage = findViewById(R.id.dice_image)
+
+    }
+
+    private fun ollDice(){
+        val randomInt = Random().nextInt(6)+1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
+    }
+}
